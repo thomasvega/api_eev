c228938f23dbfadd8437601cc97b8881b3c298a5
thomasvega <dontgonzalezvega@gmail.com>
2020-09-30 13:56:53 +0200

    Using generic class based view

--- api_eev/api/views.py
+++ api_eev/api/views.py
@@ -1,52 +1,18 @@
 from api.models import Event
 from api.serializers import EventSerializer
 
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from
+from rest_framework import generics
 
-class EventList(APIView):
+class EventList(generics.ListCreateAPIView):
     """
     List all the event or create a new event.
     """
-    def get(self, request, format=None):
-        events = Event.objects.all()
-        serializer = EventSerializer(events, many=True)
-        return Response(serializer.data)
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
 
-    def post(self, request, format=None):
-        serializer = EventSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class EventDetail(APIView):
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete an event.
     """
-    def get_object(self, pk):
-        try:
-            return Event.objects.get(pk=pk)
-        except Event.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk, format=None):
-        event = self.get_object(pk)
-        serializer = EventSerializer(event)
-        return Response(serializer.data)
-    
-    def put(self, request, pk, format=None):
-        event = self.get_object(pk)
-        serializer = EventSerializer(event, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk, format=None):
-        event = self.get_object(pk)
-        event.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
